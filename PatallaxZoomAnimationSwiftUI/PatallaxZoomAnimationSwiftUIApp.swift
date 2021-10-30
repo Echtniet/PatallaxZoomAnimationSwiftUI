fbaf348c098aa141a597c385d80a1dc1fc2f7fce
Clinton Davelaar <DavelaarProgramming@gmail.com>
2021-10-30 16:03:27 +0200

    Initial Commit

---
+++ PatallaxZoomAnimationSwiftUI/PatallaxZoomAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatallaxZoomAnimationSwiftUIApp.swift
+//  PatallaxZoomAnimationSwiftUI
+//
+//  Created by Danae Davelaar on 30/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PatallaxZoomAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
